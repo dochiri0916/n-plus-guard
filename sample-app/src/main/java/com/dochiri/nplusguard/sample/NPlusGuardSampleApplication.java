e361a61bf61a665778bafb9ab7eda03d5825723d
송성빈 <dochiri0916@gmail.com>
2026-03-10 21:09:06 +0900

    feat: 샘플 애플리케이션 추가

---
+++ sample-app/src/main/java/com/dochiri/nplusguard/sample/NPlusGuardSampleApplication.java
@@ -0,0 +1,12 @@
+package com.dochiri.nplusguard.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NPlusGuardSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NPlusGuardSampleApplication.class, args);
+    }
+}
